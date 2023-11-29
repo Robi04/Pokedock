77a3b31f584115e9130338d8cb835325649884f3
pelleservan <pelle.servan@hotmail.fr>
2023-11-29 12:31:36 +0100

    update

--- pokedock/resources/views/components/OrderItmesTable.blade.php
+++
@@ -1,5 +0,0 @@
-<tr>
-    <td>{{$order_item->id_user_order}}</td>
-    <td>{{$order_item->id_shoppack}}</td>
-    <td>{{$order_item->quantity}}</td>
-</tr>
--- pokedock/resources/views/components/ShoppacksTable.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td>{{$shoppack->id_shoppack}}</td>
-    <td>{{$shoppack->name_shoppack}}</td>
-    <td>{{$shoppack->price_shoppack}}</td>
-    <td>{{$shoppack->nb_credit_shoppack}}</td>
-    <td>{{$shoppack->path_img_shoppack}}</td>
-</tr>
