09c537ff11ac989293b9e1ce8a18ee962fc66f2d
LileFab <backspeen@gmail.com>
2023-12-06 17:56:49 +0100

    debut de la page du shop

--- pokedock/resources/views/shop/index.blade.php
+++ pokedock/resources/views/shop/index.blade.php
@@ -1,2 +1,9 @@
 <h1>Shop</h1>
 
+@foreach ($shoppacks as $sp)
+  <div style="display: inline-block; margin-right: 10px;">
+    <img src="/images/zizi.png" alt="img du shop pack" style="height:100px; width:100px">
+    <p>Nom du pack : {{$sp -> name_shoppack}}</p>
+    <button type="button">{{$sp -> price_shoppack}} €</button>
+  </div>
+@endforeach
