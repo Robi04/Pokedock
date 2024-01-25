8405eabf877cc9a71dcd6b3ce09e5a87c9af130a
pelleservan <pelle.servan@hotmail.fr>
2024-01-25 08:53:45 +0100

    update front

--- pokedock/resources/views/header.blade.php
+++ pokedock/resources/views/header.blade.php
@@ -1,16 +1,24 @@
-<header class="flex items-center justify-between bg-gray-200 p-4">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<header class="flex items-center justify-between bg-pokeBlue p-4">
     <div class="flex items-center">
-        <a href="{{ route('dashboard') }}">PokeDock</a>
+        <a class="font-pokeFont text-white text-4xl" href="{{ route('dashboard') }}">PokeDock</a>
     </div>
-    <div class="flex items-center space-x-4 border-2 shadow-2xl">
+    <div class="flex items-center space-x-4 font-mono text-2xl">
         <form action="{{ route('shop') }}" method="get">
-            <button type="submit" class="">Shop</button>
+            <button type="submit" class="text-white">Shop</button>
         </form>
         <form action="{{ route('order_items') }}" method="get">
-            <button type="submit" class="">Cart</button>
+            <button type="submit" class="text-white">Cart</button>
         </form>
         <form action="{{ route('logout') }}" method="get">
-            <button type="submit" class="btn">Log Out</button>
+            <button type="submit" class="btn text-white">Log Out</button>
         </form>
     </div>
 </header>
--- pokedock/tailwind.config.js
+++ pokedock/tailwind.config.js
@@ -5,8 +5,21 @@ module.exports = {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    plugins: [],
+
     theme: {
+        extend: {
+          fontFamily: {
+            pokeFont: 'pokeFont',
+            sigmarOne: 'sigmarOne',
+          },
+          colors: {
+            pokeBlue: 'rgb(23 89 139 / 62%)',
+            pokeBlueB: 'rgb(241 247 255)',
+          }
+        },
+      },
+    variants: {
         extend: {},
     },
-    plugins: [],
-};
+}
