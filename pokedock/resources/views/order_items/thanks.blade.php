2b85d2ac21daae7331f33fc017b7f2d533dffb90
pelleservan <pelle.servan@hotmail.fr>
2024-01-26 12:01:01 +0100

    update front

--- pokedock/resources/views/order_items/thanks.blade.php
+++ pokedock/resources/views/order_items/thanks.blade.php
@@ -4,15 +4,18 @@
     <title>Your Order</title>
     @vite('resources/css/app.css')
 </head>
-<body class="flex flex-col min-h-screen">
+<body class="flex flex-col min-h-screen bg-slate-200 font-mono">
     @include('header')
     <div class="flex-grow">
-        <h1 class="text-center font-bold text-4xl m-4">Thank you for your order !</h1>
-        <form action="{{ route('generateInvoice') }}" method="GET">
-            @csrf
-            <button type="submit">Your Invoice</button>
-        </form>
+        <h1 class="text-center font-bold text-6xl m-10 font-pokeFont text-pokeBlue">Thank you for your order !</h1>
+        <div class="m-10 text-center place-items-center text-2xl">
+            <form action="{{ route('generateInvoice') }}" method="GET">
+                @csrf
+                <button type="submit" class="text-slate-50 bg-pokeBlue p-3 rounded-2xl">Your Invoice</button>
+            </form>
+        </div>
     </div>
     @include('footer')
 </body>
 </html>
+ 
